81865889b0fc7ecd377d85db0162559f781dbdbc
Caio-Elcio <eloyduartecaio@gmail.com>
2022-07-12 20:10:11 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/img/logo.svg";
+import "./styles.css";
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" title="Dashboard" />
+                <h1>DSMeta</h1>
+                <p>
+                    Developed by
+                    <a href="https://github.com/Caio-Elcio" target="_blank"> Caio </a>
+                </p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
